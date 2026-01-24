f8066232232c8d98882c5e1b9e78b679e47add24
rafael rodriguez <dtrafa7@gmail.com>
2026-01-24 08:01:33 -0500

    pwa

---
+++ static/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'gnb-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
